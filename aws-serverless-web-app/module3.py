35ddafd2dc2f657b7ac835184ea9db7064148fbd
alic <1570970+Znapy@users.noreply.github.com>
2024-05-26 16:47:45 +0000

    add lambda, test event and clean function

--- aws-serverless-web-app/module3.py
+++ aws-serverless-web-app/module3.py
@@ -16,21 +16,31 @@ from helpers import BASE_DIR, PROJECTNAME, REPO_NAME, IAM_USER_MAIL, REGION, \
 # import module1
 # import module2
 
+
 def add_policies() -> None:
     """Add module policies to the user."""
     arns = ["arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
-            "arn:aws:iam::aws:policy/IAMFullAccess"]
+            "arn:aws:iam::aws:policy/IAMFullAccess",
+            "arn:aws:iam::aws:policy/AWSLambda_FullAccess"]
     attached_policy_arns = arns_user_policies()
     for arn in arns:
         if arn not in attached_policy_arns:
             attach_policy(arn)
 
 
-def create_dynamodb_table() -> str:
-    """Create 'Rides' - a DynamoDB table."""
-    table_name = "Rides"
-    table_arn = f"arn:aws:dynamodb:{REGION}:{ACCOUNT_ID}:table/{table_name}"
+def _get_dynamodb_stream_arn(table_name: str) -> str:
+    """Get DynamoDB stream ARN."""
+    result = run(["aws", "dynamodb", "describe-table",
+                  "--table-name", table_name,
+                  "--query", "Table.LatestStreamArn",
+                  "--profile", PROJECTNAME])
+    if result.returncode != 0:
+        raise ValueError(f"Error describe table: {result}")
+    return result.stdout.decode().strip()
 
+
+def create_dynamodb_table(table_name: str) -> str:
+    """Create 'Rides' - a DynamoDB table."""
     result = run(["aws", "dynamodb", "list-tables",
                   "--query", f"TableNames[?contains(@, '{table_name}')]",
                   "--profile", PROJECTNAME])
@@ -38,13 +48,15 @@ def create_dynamodb_table() -> str:
         raise ValueError(f"Error listing tables: {result}")
     if result.stdout.decode().strip() != "[]":
         print(f"Table '{table_name}' already exists")
-        return table_arn
+        return _get_dynamodb_stream_arn(table_name)
 
     result = run([
         "aws", "dynamodb", "create-table", "--table-name", table_name,
         "--attribute-definitions", "AttributeName=RideId,AttributeType=S",
         "--key-schema", "AttributeName=RideId,KeyType=HASH",
         "--provisioned-throughput", "ReadCapacityUnits=1,WriteCapacityUnits=1",
+        "--stream-specification",
+        "StreamEnabled=true,StreamViewType=NEW_AND_OLD_IMAGES",
         "--profile", PROJECTNAME])
     if result.returncode != 0:
         raise ValueError(f"Error creating table: {result.stderr.decode()}")
@@ -64,7 +76,7 @@ def create_dynamodb_table() -> str:
         raise ValueError(f"Error creating table, status: {status}")
 
     print(f"Table '{table_name}' has been created")
-    return table_arn
+    return _get_dynamodb_stream_arn(table_name)
 
 
 # def create_policy(name: str, document: str) -> str:
@@ -108,20 +120,77 @@ def create_inlile_policy_in_role(role_name: str, policy_name: str,
     return
 
 
-def clean() -> None:
+def create_lambda_function(role_arn: str, function_name: str) -> str:
+    """Create lambda function."""
+    result = run(["aws", "lambda", "list-functions",
+                  "--query", f"Functions[?FunctionName=='{function_name}']."
+                  "FunctionName",
+                  "--profile", PROJECTNAME])
+    if result.returncode == 0 and result.stdout.decode().strip() != "[]":
+        print(f"Lambda function {function_name} already exists")
+        return f"arn:aws:lambda:{REGION}:{ACCOUNT_ID}:function:{function_name}"
+
+    result = run(["aws", "lambda", "create-function",
+                  "--function-name", function_name,
+                  "--runtime", "nodejs16.x",
+                  "--role", role_arn,
+                  "--handler", "requestUnicorn.handler",
+                  "--zip-file", "fileb://./lambda.zip",
+                  "--profile", PROJECTNAME])
+    if result.returncode != 0:
+        raise ValueError(f"Error creating lambda function: {result}")
+    print(f"Lambda function {function_name} has been created", result)
+    return json.loads(result.stdout.decode())["FunctionArn"]
+
+
+def create_test_event(lambda_name: str) -> None:
+    """Create test event."""
+    response_file = BASE_DIR / "response.json"
+    result = run(["aws", "lambda", "invoke",
+                  "--function-name", lambda_name,
+                  "--cli-binary-format", "raw-in-base64-out",
+                  "--payload", "file://./TestRequestEvent.js",
+                  "--profile", PROJECTNAME, response_file])
+    if result.returncode != 0:
+        raise ValueError(f"Error creating event: {result}")
+    if not response_file.is_file():
+        raise ValueError(f"Error creating file {response_file}")
+
+    with open(response_file, "r") as file:
+        data = json.load(file)
+    if data["statusCode"] != 201:
+        raise ValueError(f"Error in: {response_file}")
+    response_file.unlink()
+    print(f"Test event has been checked:", data["body"])
+
+
+def clean(table_name: str, lambda_name: str) -> None:
     """Clean up in aws cloud what we have created in this module"""
+    result = run(["aws", "lambda", "delete-function",
+                  "--function-name", lambda_name, "--profile", PROJECTNAME])
+    if result.returncode != 0:
+        raise ValueError(f"Error creating event: {result}")
+
+    result = run(["aws", "dynamodb", "delete-table",
+                  "--table-name", table_name, "--profile", PROJECTNAME])
+    if result.returncode != 0:
+        raise ValueError(f"Error creating event: {result}")
+
     # TODO: clean module 3
-    # delete DynamoDB table
     # Delete role
+
 
 
 def main() -> None:
     """Main function."""
     # module1.main()
     # module2.main()
-    add_policies()
-    table_arn = create_dynamodb_table()
+    dynamodb_table_name = "Rides"
     role_name = "WildRydesLambda"
+    lambda_name = "RequestUnicorn"
+    add_policies()
+    stream_arn = create_dynamodb_table(dynamodb_table_name)
+    table_arn = stream_arn.split("/stream/")[0]
     iam_service_role_arn = create_role(
         role_name,
         '{"Version":"2012-10-17","Statement":[{"Effect":"Allow",'
@@ -133,10 +202,13 @@ def main() -> None:
         "DynamoDBWriteAccess",
         '{"Version":"2012-10-17","Statement":[{"Effect":"Allow","Action":'
         f'["dynamodb:PutItem"],"Resource":"{table_arn}"' + '}]}')
+    function_arn = create_lambda_function(iam_service_role_arn, lambda_name)
+    create_test_event(lambda_name)
 
-    #input("press Enter to delete apps (amplify)"
-    #      " or Ctrl+C to leave them running...")
-    #clean()
+    input("press Enter to delete apps (lambda, dynamoDB)"
+          " or Ctrl+C to leave them running...")
+    clean(dynamodb_table_name, lambda_name)
+    #module2.clean()
     #module1.clean()
 
 
